9d939fecae4b4b7abc2d80b0fc5ef73e93bb7510
dhairya agarwal <Dhairya07official@gmail.com>
2024-06-02 23:50:11 +0530

    display items on screen

--- sellers_app/lib/itemsScreens/items_screen.dart
+++ sellers_app/lib/itemsScreens/items_screen.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:sellers_app/itemsScreens/items_ui_design_widget.dart';
 import 'package:sellers_app/itemsScreens/upload_items_screen.dart';
+import 'package:sellers_app/models/items.dart';
+import '../global/global.dart';
 import '../models/brands.dart';
 import '../widgets/text_delegate_header_widget.dart';
 
@@ -63,10 +68,60 @@ class _ItemsScreenState extends State<ItemsScreen>
       ),
       body: CustomScrollView(
         slivers: [
+
           SliverPersistentHeader(
             pinned: true,
             delegate: TextDelegateHeaderWidget(title: "My " + widget.model!.brandTitle.toString() + "'s Items"),
           ),
+
+          //1. query
+          //2. model
+          //3. ui design widget
+
+          StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("sellers")
+                .doc(sharedPreferences!.getString("uid"))  // Retrieve sellerUid from sharedPreferences
+                .collection("brands")
+                .doc(widget.model!.brandID)  // Retrieve brandId from widget.model
+                .collection("items")
+                .orderBy("publishedDate", descending: true)
+                .snapshots(),
+            builder: (context, AsyncSnapshot dataSnapshot)
+            {
+              if(dataSnapshot.hasData) //if items exists
+                  {
+                //display items
+                return SliverStaggeredGrid.countBuilder(
+                  crossAxisCount: 1,
+                  staggeredTileBuilder: (c)=> const StaggeredTile.fit(1),
+                  itemBuilder: (context, index)
+                  {
+                    Items itemsModel = Items.fromJson(
+                      dataSnapshot.data.docs[index].data() as Map<String, dynamic>,
+                    );
+
+                    return ItemsUiDesignWidget(
+                      model: itemsModel,
+                      context: context,
+                    );
+                  },
+                  itemCount: dataSnapshot.data.docs.length,
+                );
+              }
+              else //if items NOT exists
+                  {
+                return const SliverToBoxAdapter(
+                  child: Center(
+                    child: Text(
+                      "No items exists",
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+
         ],
       ),
     );
--- sellers_app/lib/itemsScreens/items_ui_design_widget.dart
+++ sellers_app/lib/itemsScreens/items_ui_design_widget.dart
@@ -1,31 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:sellers_app/itemsScreens/items_screen.dart';
-import 'package:sellers_app/models/brands.dart';
+import 'package:sellers_app/models/items.dart';
 
-import '../models/items.dart';
+class ItemsUiDesignWidget extends StatefulWidget {
+  final Items? model;
+  final BuildContext? context;
 
-class ItemsUiDesignWidget extends StatefulWidget
-{
-  Items? model;
-  BuildContext? context;
-
-  ItemsUiDesignWidget({this.model, this.context,});
+  ItemsUiDesignWidget({this.model, this.context});
 
   @override
   State<ItemsUiDesignWidget> createState() => _ItemsUiDesignWidgetState();
 }
 
-
-
-
-class _ItemsUiDesignWidgetState extends State<ItemsUiDesignWidget>
-{
+class _ItemsUiDesignWidgetState extends State<ItemsUiDesignWidget> {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ()
-      {
+      onTap: () {
         // Navigator.push(context, MaterialPageRoute(builder: (c)=> ItemsScreen(
         //   model: widget.model,
         // )));
@@ -36,47 +26,36 @@ class _ItemsUiDesignWidgetState extends State<ItemsUiDesignWidget>
         child: Padding(
           padding: const EdgeInsets.all(4),
           child: SizedBox(
-            height: 270,
             width: MediaQuery.of(context).size.width,
-            child: Column(
-              children: [
-
-
-                Text(
-                  widget.model!.itemTitle.toString(),
-                  style: const TextStyle(
-                    color: Colors.deepPurple,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    letterSpacing: 3,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(
+                    widget.model!.itemTitle.toString(),
+                    style: const TextStyle(
+                      color: Colors.deepPurple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      letterSpacing: 3,
+                    ),
                   ),
-                ),
-
-
-                const SizedBox(height: 2,),
-                
-
-                Image.network(
-                  widget.model!.thumbnailUrl.toString(),
-                  height: 220,
-                  fit: BoxFit.cover,
-                ),
-
-                const SizedBox(height: 2,),
-
-
-                Text(
-                  widget.model!.itemInfo.toString(),
-                  style: const TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14,
-                    
+                  const SizedBox(height: 2),
+                  Image.network(
+                    widget.model!.thumbnailUrl.toString(),
+                    height: 220,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
                   ),
-                ),
-
-                
-
-              ],
+                  const SizedBox(height: 2),
+                  Text(
+                    widget.model!.itemInfo.toString(),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
