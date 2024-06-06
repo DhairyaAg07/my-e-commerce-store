ac346af5c5628635dcfa585f64ce5bd96ae62a7b
dhairya agarwal <Dhairya07official@gmail.com>
2024-06-06 22:41:30 +0530

    Carousel Image-Slider

--- users_app/lib/mainScreens/home_screen.dart
+++ users_app/lib/mainScreens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:users_app/global/global.dart';
 import 'package:users_app/widgets/my_drawer.dart';
 
 
@@ -16,6 +17,7 @@ class _HomeScreenState extends State<HomeScreen>
   Widget build(BuildContext context)
   {
     return Scaffold(
+      backgroundColor: Colors.black,
       drawer: MyDrawer(),
       appBar: AppBar(
         flexibleSpace: Container(
@@ -47,28 +49,46 @@ class _HomeScreenState extends State<HomeScreen>
         //   image slider
           
           SliverToBoxAdapter(
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height*.3,
-              width: MediaQuery.of(context).size.width,
-              
-              
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: MediaQuery.of(context).size.height*.09,
-                  aspectRatio: 16/9,
-                  viewportFraction: 0.8,
-                  initialPage: 0,
-                  enableInfiniteScroll: true,
-                  reverse: false,
-                  autoPlay: true,
-                  autoPlayInterval: Duration(seconds: 2),
-                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enlargeCenterPage: true,
-                  enlargeFactor: 0.3,
-                  scrollDirection: Axis.horizontal,
-                ), 
-                items: [],
+            child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height*.3,
+                width: MediaQuery.of(context).size.width,
+                
+                
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: MediaQuery.of(context).size.height*.25,
+                    aspectRatio: 16/9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 2),
+                    autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    scrollDirection: Axis.horizontal,
+                  ), 
+                  items: itemsImagesList.map((index)
+                  {
+                    return Builder(builder: (BuildContext c){
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 1.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(40),
+                          child: Image.asset(
+                            index,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      );
+                    });
+                  }).toList(),
+                ),
               ),
             ),
           ),
